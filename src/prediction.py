09f85e9ecd7f6ca7cc9a62bb398d5ffbe08a6804
Mahmudul Hasan Bhuiyan <mahmudulcsecu@gmail.com>
2022-09-07 23:03:12 +0200

    added the prediciton service

--- src/prediction.py
+++ src/prediction.py
@@ -1,18 +1,42 @@
+import os
+import mlflow
 import pandas as pd
+from flask import Flask, request, jsonify
 
-def read_data(path: str, date_columns: list[int], header_col:int = 0) -> pd.DataFrame:
-    return pd.read_csv(path, header=header_col, parse_dates=date_columns)
+RUN_ID = os.environ.get("RUN_ID")
+EXPERIMENT_ID = os.environ.get("EXPERIMENT_ID")
+S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
 
-def preprocess_df(input_df: pd.DataFrame)->pd.DataFrame:
-    return input_df
+if None in (RUN_ID, EXPERIMENT_ID, S3_BUCKET_NAME):
+    raise ValueError("AWS configuration is not set in the environment variables")
+
+# load model from S3
+model_url = f"s3://{S3_BUCKET_NAME}/{EXPERIMENT_ID}/{RUN_ID}/artifacts/models"
+model = mlflow.pyfunc.load_model(model_url)
 
 
-ride_path = "data/2022-07-01/20220107_donnees_ouvertes.csv"
-stations_path = "data/2022-07-01/20220107_stations.csv"
+def predict(features):
+    preds = model.predict(features)
+    return float(preds[0])
 
-ride_df = read_data(ride_path, [0, 2], 0)
-stations_df = read_data(stations_path, [], 0)
 
-print(ride_df.head())
-print(ride_df.dtypes)
-print(stations_df.head())
+# create the flask app
+bixi_app = Flask('bixi-ride-duration-prediction')
+
+# create the prediciton endpoint
+@bixi_app.route('/predict', methods=['POST'])
+def duration_prediction():
+
+    trip_details = request.get_json()
+    duration = predict(trip_details)
+
+    prediction = {
+        'duration_minute': duration,
+        'model_version': RUN_ID
+    }
+
+    return jsonify(prediction)
+
+# start the flask app
+if __name__ == "__main__":
+    bixi_app.run(debug=True, host='0.0.0.0', port=9696)
